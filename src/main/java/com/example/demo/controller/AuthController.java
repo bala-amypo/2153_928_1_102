c3a14dd80162dcc9e75f770ff09209e8131e81df
bala-amypo <bala@amypo.in>
2025-12-19 05:12:31 +0000

    User commit

--- src/main/java/com/example/demo/controller/AuthController.java
+++ src/main/java/com/example/demo/controller/AuthController.java
@@ -15,18 +15,13 @@ public class AuthController {
 
     @PostMapping("/register")
     public User register(@RequestBody User user){ 
-        return userService.saveUser(user); 
+        return userService.register(user); 
         }
 
-    @GetMapping("/all")
-    public List<User> getUsers(){ return userService.getAllUsers(); }
+        @PostMapping("/login")
+    public User login(@RequestBody User user){ 
+        return userService.login(user); 
+        }
 
-    @GetMapping("/{id}")
-    public User getUser(@PathVariable Long id){ return userService.getUserById(id); }
-
-    @DeleteMapping("/{id}")
-    public String deleteUser(@PathVariable Long id){
-        userService.deleteUser(id);
-        return "Deleted successfully";
-    }
+   
 }
