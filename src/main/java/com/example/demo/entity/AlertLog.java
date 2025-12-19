3d0d523ab7914c32d102b84dd8b85938a133e85c
bala-amypo <bala@amypo.in>
2025-12-19 08:08:57 +0000

    User commit

--- src/main/java/com/example/demo/entity/AlertLog.java
+++ src/main/java/com/example/demo/entity/AlertLog.java
@@ -1,6 +1,8 @@
 package com.example.demo.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.*;
 import java.time.LocalDateTime;
 
@@ -12,10 +14,16 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 @Builder
 public class AlertLog {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotBlank(message = "Alert message must not be empty")
+    @Column(nullable = false)
     private String message;
+
+    @NotNull(message = "Created time is required")
+    @Column(nullable = false)
     private LocalDateTime createdAt;
 }
