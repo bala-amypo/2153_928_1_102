e0a791db890641d083b7ee7b2ea1b5a4927f227b
bala-amypo <bala@amypo.in>
2025-12-19 05:57:57 +0000

    User commit

--- src/main/java/com/example/demo/service/UserServiceImpl.java
+++ src/main/java/com/example/demo/service/UserServiceImpl.java
@@ -1,29 +1,37 @@
 package com.example.demo.service.impl;
 
 import org.springframework.stereotype.Service;
-import java.util.List;
 import com.example.demo.entity.User;
 import com.example.demo.repository.UserRepository;
 import com.example.demo.service.UserService;
 
 @Service
 public class UserServiceImpl implements UserService {
+
     private final UserRepository userRepository;
-    public UserServiceImpl(UserRepository userRepository){ this.userRepository = userRepository; }
+
+    // Constructor Injection (MANDATORY)
+    public UserServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
 
     @Override
-    public User saveUser(User user){
-        if(userRepository.existsByEmail(user.getEmail()))
-            throw new RuntimeException("Email exists");
+    public User register(User user) {
+
+        if (userRepository.existsByEmail(user.getEmail())) {
+            throw new RuntimeException("email already exists");
+        }
+
+        if (user.getRole() == null) {
+            user.setRole("USER");
+        }
+
         return userRepository.save(user);
     }
 
     @Override
-    public List<User> getAllUsers(){ return userRepository.findAll(); }
-
-    @Override
-    public User getUserById(Long id){ return userRepository.findById(id).orElseThrow(() -> new RuntimeException("User not found")); }
-
-    @Override
-    public void deleteUser(Long id){ userRepository.deleteById(id); }
+    public User findByEmail(String email) {
+        return userRepository.findByEmail(email)
+                .orElseThrow(() -> new RuntimeException("User not found"));
+    }
 }
