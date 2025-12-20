565b795712aa07e46023687a544029f160cb13b8
bala-amypo <bala@amypo.in>
2025-12-20 14:37:50 +0000

    User commit

--- src/main/java/com/example/demo/service/AlertScheduleServiceImpl.java
+++ src/main/java/com/example/demo/service/AlertScheduleServiceImpl.java
@@ -19,3 +19,13 @@ public class AlertScheduleServiceImpl implements AlertScheduleService {
 
     
 }
+package com.example.demo.service;
+
+import com.example.demo.entity.AlertSchedule;
+import java.util.List;
+
+public interface AlertScheduleService {
+    AlertSchedule saveSchedule(AlertSchedule s);
+    List<AlertSchedule> getAllSchedules();
+    
+}
