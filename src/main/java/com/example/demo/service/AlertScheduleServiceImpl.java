ef2d66d2fe3c619ea5f7787c41184f71bd23397c
bala-amypo <bala@amypo.in>
2025-12-20 14:40:50 +0000

    User commit

--- src/main/java/com/example/demo/service/AlertScheduleServiceImpl.java
+++ src/main/java/com/example/demo/service/AlertScheduleServiceImpl.java
@@ -1,31 +1,41 @@
 package com.example.demo.service.impl;
 
-import org.springframework.stereotype.Service;
-import java.util.List;
 import com.example.demo.entity.AlertSchedule;
+import com.example.demo.entity.Warranty;
 import com.example.demo.repository.AlertScheduleRepository;
+import com.example.demo.repository.WarrantyRepository;
 import com.example.demo.service.AlertScheduleService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class AlertScheduleServiceImpl implements AlertScheduleService {
-    private final AlertScheduleRepository repo;
-    public AlertScheduleServiceImpl(AlertScheduleRepository repo){ this.repo = repo; }
+
+    private final AlertScheduleRepository alertRepo;
+    private final WarrantyRepository warrantyRepo;
+
+    public AlertScheduleServiceImpl(AlertScheduleRepository alertRepo,
+                                    WarrantyRepository warrantyRepo) {
+        this.alertRepo = alertRepo;
+        this.warrantyRepo = warrantyRepo;
+    }
 
     @Override
-    public AlertSchedule saveSchedule(AlertSchedule s){ return repo.save(s); }
+    public AlertSchedule saveSchedule(AlertSchedule s) {
+
+        Long warrantyId = s.getWarranty().getId();
+
+        Warranty warranty = warrantyRepo.findById(warrantyId)
+                .orElseThrow(() -> new RuntimeException("Warranty not found"));
+
+        s.setWarranty(warranty);
+
+        return alertRepo.save(s);
+    }
 
     @Override
-    public List<AlertSchedule> getAllSchedules(){ return repo.findAll(); }
-
-    
-}
-package com.example.demo.service;
-
-import com.example.demo.entity.AlertSchedule;
-import java.util.List;
-
-public interface AlertScheduleService {
-    AlertSchedule saveSchedule(AlertSchedule s);
-    List<AlertSchedule> getAllSchedules();
-    
+    public List<AlertSchedule> getAllSchedules() {
+        return alertRepo.findAll();
+    }
 }
