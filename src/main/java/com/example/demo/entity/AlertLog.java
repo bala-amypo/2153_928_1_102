858cdcf5ea797b773f4bbc32e09132b4275b1211
bala-amypo <bala@amypo.in>
2025-12-26 05:56:45 +0000

    User commit

--- src/main/java/com/example/demo/entity/AlertLog.java
+++ src/main/java/com/example/demo/entity/AlertLog.java
@@ -1,22 +1,28 @@
+package com.example.demo.entity;
+
 import jakarta.persistence.*;
 import lombok.*;
+import java.time.LocalDateTime;
 
-@Entity
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
+@Entity
 public class AlertLog {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String message;
+
     private LocalDateTime alertTime;
 
     @ManyToOne
     private Warranty warranty;
 
+    // Used to set alert time automatically
     public void prePersist() {
         this.alertTime = LocalDateTime.now();
     }
