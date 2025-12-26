80edca9b0c28312fe1e26a8410acd222d16fd40a
bala-amypo <bala@amypo.in>
2025-12-26 06:23:46 +0000

    User commit

--- src/main/java/com/example/demo/service/AlertLogServiceImpl.java
+++ src/main/java/com/example/demo/service/AlertLogServiceImpl.java
@@ -24,7 +24,7 @@ public class AlertLogServiceImpl implements AlertLogService {
     public AlertLog addLog(Long warrantyId, String alertmessage){
         warrantyRepository.findById(warrantyId).orElseThrow(() -> new RuntimeException("Warranty not found"));
 
-        AlertLog log = AlertLog.builder()..build();
+        AlertLog log = AlertLog.builder().alertMessage("Warranty expiring soon").build();
         log.prePersist();
         return alertLogRepository.save(log);
     }
