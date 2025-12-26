3475232268b573e2ebdc920f53cf461db0984529
bala-amypo <bala@amypo.in>
2025-12-26 04:56:46 +0000

    User commit

--- src/main/java/com/example/demo/entity/Product.java
+++ src/main/java/com/example/demo/entity/Product.java
@@ -1,13 +1,27 @@
 package com.example.demo.entity;
 
-import lombok.*;
-
-@Getter @Setter
-@AllArgsConstructor @NoArgsConstructor @Builder
 public class Product {
+
     private Long id;
     private String name;
     private String brand;
     private String modelNumber;
     private String category;
+
+    public Product() {}
+
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+
+    public String getBrand() { return brand; }
+    public void setBrand(String brand) { this.brand = brand; }
+
+    public String getModelNumber() { return modelNumber; }
+    public void setModelNumber(String modelNumber) { this.modelNumber = modelNumber; }
+
+    public String getCategory() { return category; }
+    public void setCategory(String category) { this.category = category; }
 }
